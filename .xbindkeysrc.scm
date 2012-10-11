b7a0a1aa0bf4ef3eb2cc288c8afc1580f00fd66a
Nicolas LaCasse <nlacasse@borderstylo.com>
2012-10-11 15:29:15 -0700

    volumn keys

---
+++ .xbindkeysrc.scm
@@ -0,0 +1,3 @@
+(xbindkey '("XF86Sleep") "sudo pm-suspend")
+(xbindkey '("XF86AudioRaiseVolume") "amixer set Master 2dB+ unmute")
+(xbindkey '("XF86AudioLowerVolume") "amixer set Master 2dB- unmute")
