081db5caaa35781b1fda0974fd11c9cbf1133e56
Nicolas LaCasse <nlacasse@borderstylo.com>
2012-10-25 15:01:00 -0700

    the hardware button toggles colemak

--- .xbindkeysrc.scm
+++ .xbindkeysrc.scm
@@ -1,4 +1,4 @@
 ;(xbindkey '("XF86Sleep") "sudo pm-suspend")
 (xbindkey '("XF86AudioRaiseVolume") "amixer set Master 2dB+ unmute")
 (xbindkey '("XF86AudioLowerVolume") "amixer set Master 2dB- unmute")
-(xbindkey '("XF86Launch1") "synclient TouchpadOff=$(synclient -l | grep -c 'TouchpadOff.*=.*0')")
+(xbindkey '("XF86Launch1") "~/bin/togglemak")
