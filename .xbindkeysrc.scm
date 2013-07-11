38726aff34dafbd8a34bdc9d88fdc1ab136aa82c
Nicolas LaCasse <nlacasse@borderstylo.com>
2013-07-10 21:13:26 -0700

    toggle touchpad with bar button

--- .xbindkeysrc.scm
+++ .xbindkeysrc.scm
@@ -1,4 +1,4 @@
 ;(xbindkey '("XF86Sleep") "sudo pm-suspend")
 (xbindkey '("XF86AudioRaiseVolume") "amixer set Master 2dB+ unmute")
 (xbindkey '("XF86AudioLowerVolume") "amixer set Master 2dB- unmute")
-(xbindkey '("XF86Launch1") "~/bin/togglemak")
+(xbindkey '("XF86Launch1") "~/.togglemak")
